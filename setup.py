9f8b6d508db1599dfda24daab65e0e98ec6da5ca
irg <yang@eecs.berkeley.edu>
2019-08-28 21:42:23 -0700

    create irmark1 in setup.py

--- setup.py
+++ setup.py
@@ -22,17 +22,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setup(name='donkeycar',
-    version='3.1.0',
+setup(name='irmark1',
+    version='1.0',
     long_description = long_description,
-    description='Self driving library for python.',
-    url='https://github.com/autorope/donkeycar',
-    author='Will Roscoe, Adam Conway, Tawn Kramer',
-    author_email='wroscoe@gmail.com, adam@casaconway.com, tawnkramer@gmail.com',
-    license='MIT',
+    description='Intelligent Racing Mark One Python Package.',
+    url='https://github.com/augcog/IRG',
+    author='Allen Y. Yang',
+    author_email='yang@eecs.berkeley.edu',
+    license='Apache v2.0',
     entry_points={
         'console_scripts': [
-            'donkey=donkeycar.management.base:execute_from_command_line',
+            'irmark1=irmark1.management.base:execute_from_command_line',
         ],
     },
     install_requires=['numpy', 
@@ -48,11 +48,6 @@ setup(name='donkeycar',
                      ],
 
     extras_require={
-                    'pi': [
-                        'picamera',
-                        'Adafruit_PCA9685',
-                        'RPi.GPIO'
-                        ],
                     'nano': [
                         'Adafruit_PCA9685',                        
                         ],
@@ -69,7 +64,7 @@ setup(name='donkeycar',
                     'tf_gpu': ['tensorflow-gpu>=1.9.0'],
                     },
     package_data={
-        'donkeycar': extra_files, 
+        'irmark1': extra_files, 
         },
 
       include_package_data=True,
@@ -86,7 +81,7 @@ setup(name='donkeycar',
           'Topic :: Scientific/Engineering :: Artificial Intelligence',
 
           # Pick your license as you wish (should match "license" above)
-          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: Apache License',
 
           # Specify the Python versions you support here. In particular, ensure
           # that you indicate whether you support Python 2, Python 3 or both.
@@ -94,7 +89,7 @@ setup(name='donkeycar',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
       ],
-      keywords='selfdriving cars donkeycar diyrobocars',
+      keywords='selfdriving cars',
 
       packages=find_packages(exclude=(['tests', 'docs', 'site', 'env'])),
       )
